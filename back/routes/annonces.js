a69f186314cefe147e4539b7c79015f7cdba39c9
mcharbi00 <92585972+mcharbi00@users.noreply.github.com>
2023-02-25 01:26:25 +0100

    ajouts de routes de register et de login et encryption du mot de passe avant stockage

--- back/routes/annonces.js
+++ back/routes/annonces.js
@@ -1,26 +1,84 @@
 const express = require('express');
-const Annonce = require('../models/Annonce');
-const app = express();
 const router = express.Router();
 
-// Créer une nouvelle annonce
-app.post('/annonces', (req, res) => {
-    const annonce = new Annonce({
-        titre: req.body.titre,
-        description: req.body.description,
-        prix: req.body.prix,
-        utilisateurId: req.body.utilisateurId
-    });
-    annonce.save((err, result) => {
-        if (err) {
-            console.log('Erreur lors de la création de l\'annonce :', err);
-            res.status(500).send('Erreur lors de la création de l\'annonce');
-        } else {
-            res.send(result);
-        }
-    });
-});
+// Importer le modèle d'annonce
+const Ad = require('../models/Ad');
 
 // Récupérer toutes les annonces
+router.get('/ads', async (req, res) => {
+    try {
+        const ads = await Ad.find();
+        res.json(ads);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Créer une nouvelle annonce
+router.post('/ads', async (req, res) => {
+    const ad = new Ad({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        date: req.body.date,
+        // Ajouter l'identifiant de l'utilisateur qui a créé l'annonce
+        createdBy: req.user._id
+    });
+
+    try {
+        const newAd = await ad.save();
+        res.status(201).json(newAd);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Récupérer une annonce spécifique
+router.get('/ads/:id', getAd, (req, res) => {
+    res.json(res.ad);
+});
+
+// Mettre à jour une annonce
+router.patch('/ads/:id', getAd, async (req, res) => {
+    if (req.body.title != null) {
+        res.ad.title = req.body.title;
+    }
+    if (req.body.description != null) {
+        res.ad.description = req.body.description;
+    }
+    if (req.body.price != null) {
+        res.ad.price = req.body.price;
+    }
+    try {
+        const updatedAd = await res.ad.save();
+        res.json(updatedAd);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Supprimer une annonce
+router.delete('/ads/:id', getAd, async (req, res) => {
+    try {
+        await res.ad.remove();
+        res.json({ message: 'Annonce supprimée' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Middleware pour récupérer une annonce spécifique par son identifiant
+async function getAd(req, res, next) {
+    try {
+        const ad = await Ad.findById(req.params.id);
+        if (ad == null) {
+            return res.status(404).json({ message: 'Annonce non trouvée' });
+        }
+        res.ad = ad;
+        next();
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+}
 
 module.exports = router;
--- back/routes/users.js
+++ back/routes/users.js
@@ -1,27 +1,59 @@
-
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
-const app = express();
+
 // Créer un nouvel utilisateur
-app.post('/register', (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    user.save((err, result) => {
-        if (err) {
-            console.log('Erreur lors de la création de l\'utilisateur :', err);
-            res.status(500).send('Erreur lors de la création de l\'utilisateur');
-        } else {
-            res.send(result);
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Vérification si l'email est déjà utilisé
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(409).json({ message: "L'email est déjà utilisé." });
         }
-    });
+
+        // Encryption du mot de passe
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        const newUser = await user.save();
+
+        res.status(201).json(newUser);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur lors de la création de l'utilisateur." });
+    }
+});
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ message: 'Email ou mot de passe incorrect' });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: 'Email ou mot de passe incorrect' });
+        }
+
+        res.status(200).json({ message: 'Login réussi' });
+    } catch (err) {
+        console.log('Erreur lors de la connexion de l\'utilisateur :', err);
+        res.status(500).send('Erreur lors de la connexion de l\'utilisateur');
+    }
 });
 
 // Récupérer tous les utilisateurs
-app.get('/users', (req, res) => {
+router.get('/users', (req, res) => {
     User.find({}, (err, result) => {
         if (err) {
             console.log('Erreur lors de la récupération des utilisateurs :', err);
@@ -33,7 +65,7 @@ app.get('/users', (req, res) => {
 });
 
 // Récupérer un utilisateur par son identifiant
-app.get('/users/:id', (req, res) => {
+router.get('/users/:id', (req, res) => {
     User.findById(req.params.id, (err, result) => {
         if (err) {
             console.log('Erreur lors de la récupération de l\'utilisateur :', err);
@@ -45,7 +77,7 @@ app.get('/users/:id', (req, res) => {
 });
 
 // Mettre à jour un utilisateur
-app.put('/users/:id', (req, res) => {
+router.put('/users/:id', (req, res) => {
     User.findByIdAndUpdate(req.params.id, {
         name: req.body.name,
         email: req.body.email,
@@ -61,7 +93,7 @@ app.put('/users/:id', (req, res) => {
 });
 
 // Supprimer un utilisateur
-app.delete('/users/:id', (req, res) => {
+router.delete('/users/:id', (req, res) => {
     User.findByIdAndDelete(req.params.id, (err, result) => {
         if (err) {
             console.log('Erreur lors de la suppression de l\'utilisateur :', err);
@@ -71,4 +103,5 @@ app.delete('/users/:id', (req, res) => {
         }
     });
 });
+
 module.exports = router;
